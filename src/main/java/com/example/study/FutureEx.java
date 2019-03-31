d18157d47453754b7e9e7a3004ad99311d18d22d
KimJongMin <kdhx92@gmail.com>
2019-03-31 21:07:05 +0900

    Simple FutureTask Example2

--- src/main/java/com/example/study/FutureEx.java
+++ src/main/java/com/example/study/FutureEx.java
@@ -7,23 +7,34 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 
+/*
+비동기 작업의 결과를 가져오는 방법은 Future와 같은 handler를 이용하거나 callback을 이용하는 방법이 있다.
+ */
+
 @Slf4j
 public class FutureEx {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) {
         ExecutorService es = Executors.newCachedThreadPool();
 
-        FutureTask<String> f = new FutureTask<>(() -> {
+        FutureTask<String> f = new FutureTask<String>(() -> {
             Thread.sleep(2000);
             log.info("Async");
             return "Hello";
-        });
+        }) {
+            @Override
+            protected void done() {
+                super.done();
+                try {
+                    log.info(get());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
 
         es.execute(f);
-
-        log.info(String.valueOf(f.isDone()));
-        Thread.sleep(2000);
-        log.info("Exit");
-        log.info(String.valueOf(f.isDone()));
-        log.info(f.get());
+        es.shutdown();
     }
 }
