5f023a80984533536d72212d4d6065951ad7021a
KimJongMin <kdhx92@gmail.com>
2019-03-31 21:24:07 +0900

    Implement CallbackFutureTask

--- src/main/java/com/example/study/FutureEx.java
+++ src/main/java/com/example/study/FutureEx.java
@@ -2,37 +2,44 @@ package com.example.study;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.FutureTask;
-
-/*
-비동기 작업의 결과를 가져오는 방법은 Future와 같은 handler를 이용하거나 callback을 이용하는 방법이 있다.
- */
+import java.util.Objects;
+import java.util.concurrent.*;
 
 @Slf4j
 public class FutureEx {
+    interface SuccessCallback {
+        void onSuccess(String result);
+    }
+
+    public static class CallbackFutureTask extends FutureTask<String> {
+        SuccessCallback sc;
+
+        public CallbackFutureTask(Callable<String> callable, SuccessCallback sc) {
+            super(callable);
+            this.sc = Objects.requireNonNull(sc);
+        }
+
+        @Override
+        protected void done() {
+            super.done();
+            try {
+                this.sc.onSuccess(get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         ExecutorService es = Executors.newCachedThreadPool();
 
-        FutureTask<String> f = new FutureTask<String>(() -> {
+        CallbackFutureTask f = new CallbackFutureTask(() -> {
             Thread.sleep(2000);
             log.info("Async");
             return "Hello";
-        }) {
-            @Override
-            protected void done() {
-                super.done();
-                try {
-                    log.info(get());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
+        }, log::info);
 
         es.execute(f);
         es.shutdown();
