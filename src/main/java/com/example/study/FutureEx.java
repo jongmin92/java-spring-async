0ba0988ef7a630ea5738c6d878b6ba78618b79df
KimJongMin <kdhx92@gmail.com>
2019-03-31 20:56:16 +0900

    Simple Future Example2

--- src/main/java/com/example/study/FutureEx.java
+++ src/main/java/com/example/study/FutureEx.java
@@ -7,11 +7,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-/*
-callable은 runnable하고 다르게 값을 return 수 있다. 또한 예외가 발생했을 때, 예외가 발생했을 때 다 처리하지 않고 밖으로 던질 수 있다.
-Future를 통해서 비동기 결과의 값을 가져올 수 있지만 get 메서드를 호출하게 되면 비동기 작업이 완료될 때까지 해당 스레드가 blocking된다.
- */
-
 @Slf4j
 public class FutureEx {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
@@ -23,7 +18,10 @@ public class FutureEx {
             return "Hello";
         });
 
-        log.info(f.get());
+        log.info(String.valueOf(f.isDone()));
+        Thread.sleep(2000);
         log.info("Exit");
+        log.info(String.valueOf(f.isDone()));
+        log.info(f.get());
     }
 }
