537ab947578ed0563bf7b792bf2310aea8688912
KimJongMin <kdhx92@gmail.com>
2019-03-31 21:01:56 +0900

    Simple FutureTask Example

--- src/main/java/com/example/study/FutureEx.java
+++ src/main/java/com/example/study/FutureEx.java
@@ -5,18 +5,20 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 @Slf4j
 public class FutureEx {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService es = Executors.newCachedThreadPool();
 
-        Future<String> f = es.submit(() -> {
+        FutureTask<String> f = new FutureTask<>(() -> {
             Thread.sleep(2000);
             log.info("Async");
             return "Hello";
         });
+
+        es.execute(f);
 
         log.info(String.valueOf(f.isDone()));
         Thread.sleep(2000);
