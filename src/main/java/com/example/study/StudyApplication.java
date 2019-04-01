70726d07ed7b686b8c5633d40470440bd69aad8e
KimJongMin <kdhx92@gmail.com>
2019-04-01 22:59:45 +0900

    Servlet Simple Async

--- src/main/java/com/example/study/StudyApplication.java
+++ src/main/java/com/example/study/StudyApplication.java
@@ -1,70 +1,51 @@
 package com.example.study;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+
+/*
+ Servlet 3.0: 비동기 서블릿
+  - HTTP connection은 이미 논블록킹 IO
+  - 서블릿 요청 읽기, 응답 쓰기는 블록킹
+  - 비동기 작업 시작 즉시 서블릿 쓰레드 반납
+  - 비동기 작업이 완료되면 서블릿 쓰레드 재할당
+  - 비동기 서블릿 컨텍스트 이용 (AsyncContext)
+ Servlet 3.1: 논블록킹 IO
+  - 논블록킹 서블릿 요청, 응답 처리
+  - Callback
+
+  쓰레드가 블록되는 상황은 CPU와 메모리 자원을 많이 먹는다. 컨텍스트 스위칭이 일어나기 때문.
+  기본적으로 블록이 되면 wating 상태로 빠지고 추후 running 상태로 변경되면서 컨텍스트 스위칭이 2번 일어나면서 불필요하게
+  CPU 자원이 소모된다.
+  Java InputStream과 OutputStream은 블록킹 방식이다. RequestHttpServletRequest, RequestHttpServletResponse는
+  InputSream과 OutputStream을 사용하기 때문에 서블릿은 기본적으로 블로킹 IO 방식이다.
+ */
 
 @SpringBootApplication
 @EnableAsync
 @Slf4j
 public class StudyApplication {
 
-    @Service
-    public static class MyService {
-        /*
-         기본적으로 SimpleAsyncTaskExecutor를 사용한다. 스레드를 계속 새로 만들어 사용하기 때문에 비효율적이다.
-         */
-        @Async
-//        @Async("tp")
-        public ListenableFuture<String> hello() throws InterruptedException {
-            log.info("hello()");
-            Thread.sleep(1000);
-            return new AsyncResult<>("Hello");
+    @RestController
+    public static class MyController {
+        @GetMapping("/callable")
+        public Callable<String> callable() {
+            log.info("callable");
+            return () -> {
+                log.info("async");
+                Thread.sleep(2000);
+                return "hello";
+            };
         }
-    }
-
-    @Bean
-    ThreadPoolTaskExecutor tp() {
-        ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
-        // 1) 스레드 풀을 해당 개수까지 기본적으로 생성함. 처음 요청이 들어올 때 poll size만큼 생성한다.
-        te.setCorePoolSize(10);
-        // 2) 지금 당장은 Core 스레드를 모두 사용중일때, 큐에 만들어 대기시킨다.
-        te.setQueueCapacity(50);
-        // 3) 대기하는 작업이 큐에 꽉 찰 경우, 풀을 해당 개수까지 더 생성한다.
-        te.setMaxPoolSize(100);
-        te.setThreadNamePrefix("myThread");
-        return te;
     }
 
     public static void main(String[] args) {
-        // try with resource 블록을 이용해 빈이 다 준비된 후 종료되도록 설정
-        try (ConfigurableApplicationContext c = SpringApplication.run(StudyApplication.class, args)) {
-        }
-    }
-
-    @Autowired
-    MyService myService;
-
-    // 모든 빈이 다 준비된 후 실행됨 (현재는 일종의 컨트롤러라고 생각)
-    @Bean
-    ApplicationRunner run() {
-        return args -> {
-            log.info("run()");
-            ListenableFuture<String> f = myService.hello();
-            f.addCallback(s -> log.info(s), e-> log.info(e.getMessage()));
-            log.info("exit");
-
-            Thread.sleep(2000);
-        };
+        SpringApplication.run(StudyApplication.class, args);
     }
 }
