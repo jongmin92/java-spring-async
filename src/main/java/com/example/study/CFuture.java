fb77e1259bb4f58687626b25b28cd1a54f4eddc4
KimJongMin <kdhx92@gmail.com>
2019-05-03 01:47:50 +0900

    supplyAsync, thenApply, thenAccept

--- src/main/java/com/example/study/CFuture.java
+++ src/main/java/com/example/study/CFuture.java
@@ -13,9 +13,17 @@ public class CFuture {
   public static void main(String[] args) throws ExecutionException, InterruptedException {
     // Async 작업이 끝나고 해당 스레드에서 계속해서 작업을 수행한다.
     CompletableFuture
-      .runAsync(() -> log.info("runAsync"))
-      .thenRun(() -> log.info("thenRun"))
-      .thenRun(() -> log.info("thenRun"));
+      .supplyAsync(() -> {
+        log.info("supplyAsync");
+        return 1;
+      })
+      // 앞의 비동기 작업의 결과를 받아 사용해 새로운 값을 return 한다.
+      .thenApply(s -> {
+        log.info("thenApply {}", s);
+        return s + 1;
+      })
+      // 앞의 비동기 작업의 결과를 받아 사용하며 return이 없다.
+      .thenAccept(s -> log.info("thenAccept {}", s));
     log.info("exit");
 
     // 별도의 pool을 설정하지않으면 자바7 부터는 ForkJoinPool이 자동으로 사용된다.
